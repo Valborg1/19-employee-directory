abe8201769fb7a05262ee175f51af27aecf947f1
Jared Lim <jaredlim1@gmail.com>
2021-02-11 10:43:47 -0700

    added sorting

--- src/pages/Employees/index.js
+++ src/pages/Employees/index.js
@@ -2,19 +2,48 @@ import React, { Component } from "react";
 import "./style.css";
 
 import API from "../../utils/API";
+// import Sort from "../../utils/Sort";
 
 import Container from "../../components/Container";
 import SearchForm from "../../components/SearchForm";
 import EmployeeData from "../../components/EmployeeData";
 import Alert from "../../components/Alert";
 
+function fetchFromObject(obj, prop) {
+
+  if(typeof obj === 'undefined') {
+      return false;
+  }
+
+  var _index = prop.indexOf('.')
+  if(_index > -1) {
+      return fetchFromObject(obj[prop.substring(0, _index)], prop.substr(_index + 1));
+  }
+
+  return obj[prop];
+}
+
+
 class Employee extends Component {
 state = {
   employeeData: [],
   filteredEmployeeData: [],
   search: '',
+  order: 'descend',
   error: '',
 }
+
+  flipSort() {
+    if (this.state.order === "descend") {
+      this.setState({
+        order: "ascend"
+      })
+    } else {
+      this.setState({
+        order: "descend"
+      })
+    }
+  }
 
   componentDidMount() {
     API.getEmployeeList()
@@ -26,8 +55,9 @@ state = {
   }
 
   searchEmployee = (filter) => {
-    console.log('Search', filter);
+    // console.log('Search', filter);
     const filteredList = this.state.employeeData.filter((employee) => {
+
       // merge data together, then check to see if employee exists
       let values = Object.values(employee).join('').toLowerCase();
       return values.indexOf(filter.toLowerCase()) !== -1;
@@ -40,7 +70,38 @@ state = {
     this.setState({ search: event.target.value });
     this.searchEmployee(this.state.search)
   };
- 
+
+  sort = (header) => {
+    // console.log('SORTING!!!!')
+    const {employeeData} = this.state
+    const sorted = employeeData.sort((a, b) => {
+          const x = fetchFromObject(a, header);
+          const y = fetchFromObject(b, header);
+    if( this.state.order === 'ascend') { 
+          if (x < y){
+            return -1
+        } else if (x > y){
+            return 1
+        } else {
+          return 0
+        }
+      } else { 
+        if (x < y){
+          return 1
+      } else if (x > y){
+          return -1
+      } else {
+        return 0
+      }
+    }
+    })
+    console.log('Sorted List: ', sorted)
+    this.setState({filteredEmployeeData: sorted})
+    this.flipSort()
+}
+
+
+
   render () {
     return (
       <div>
@@ -63,12 +124,12 @@ state = {
         <thead>
             <tr>
             <th scope="col" >Image</th>
-            <th scope="col" value="firstName" onClick={this.doSort}>First Name</th>
-            <th scope="col" value="lastName">Last Lame</th>
-            <th scope="col" value="email">Email</th>
-            <th scope="col" value="phoneNumber">Phone Number</th>
-            <th scope="col" value="city">City</th>
-            <th scope="col" value="SSN">SSN</th>
+            <th scope="col" value="firstName" onClick={() => this.sort('name.first')}>First Name</th>
+            <th scope="col" value="lastName"  onClick={() => this.sort('name.last')}>Last Lame</th>
+            <th scope="col" value="email" onClick={() => this.sort('email')}>Email</th>
+            <th scope="col" value="phoneNumber" onClick={() => this.sort('cell')}>Phone Number</th>
+            <th scope="col" value="city" onClick={() => this.sort('location.city')}>City</th>
+            <th scope="col" value="SSN" onClick={() => this.sort('id.value')}>SSN</th>
             </tr>
         </thead>
         <tbody>
