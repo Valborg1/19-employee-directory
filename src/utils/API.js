a6f78e94c48a8d00b19e6a4af245ec260a6b1f37
Jared Lim <jaredlim1@gmail.com>
2021-02-11 10:45:58 -0700

    updated api

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,6 +3,6 @@ import axios from "axios";
 // eslint-disable-next-line
 export default {
     getEmployeeList: function() {
-      return axios.get("https://randomuser.me/api/?results=10&nat=us");;
+      return axios.get("https://randomuser.me/api/?results=200&nat=us");;
   },
 };
